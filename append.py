253c590ce70c8ec0d5deef32e86be83d7e898e59
Aravinthanucep <36106788+Aravinthanucep@users.noreply.github.com>
2018-02-11 22:27:32 +0530

    Add files via upload

---
+++ append.py
@@ -0,0 +1,19 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Deepa
+#
+# Created:     02/02/2018
+# Copyright:   (c) Deepa 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+ var1 = str(input("enter first string"))
+ var2 = str(input("enter another string"))
+ var3 = var1 + var2
+ print(var3)
+
+if __name__ == '__main__':
+    main()
